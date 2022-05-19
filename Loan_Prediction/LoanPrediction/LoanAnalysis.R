4762d265cccc91d45f033b5837ec1ecee8ab6c55
apowell18 <alexis_ceres@hotmail.com>
2022-05-19 09:44:32 -0400

    Delete LoanAnalysis.R

--- Loan_Prediction/LoanPrediction/LoanAnalysis.R
+++
@@ -1,9 +0,0 @@
-#Load dataset
-loan_data <- read.csv('~/Documents/Projects/Data Analyst/R/Loan_Prediction/LoanDataset.csv')
-
-#################
-# Install Packages
-# Data Analysis & Visualization
-install.packages("skimr")
-library(skimr)
-#################
